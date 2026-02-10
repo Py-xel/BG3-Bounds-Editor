0139227763ede60001c6275da8c9e6f81922181e
Py-xel <szalanczy.szilard28@gmail.com>
2026-02-10 20:52:50 +0100

    feat: Add Error handling at startup

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,13 +1,27 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace BG3_Bounds_Editor;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
-}
+    public App()
+    {
+        this.DispatcherUnhandledException += (s, e) =>
+        {
+            MessageBox.Show($"Unhandled Error: {e.Exception.Message}\n\nStack Trace: {e.Exception.StackTrace}", "Critical Error");
+            e.Handled = true;
+        };
+    }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        try
+        {
+            base.OnStartup(e);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Startup Error: {ex.Message}");
+        }
+    }
+}
